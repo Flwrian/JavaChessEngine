d4ed4e10ba09ccc0b48a8496593542d95df47b8a
Florian <floflomontage@gmail.com>
2023-04-16 16:31:10 +0200

    Added RandomAlgorithm

---
+++ src/main/java/com/algorithms/RandomAlgorithm.java
@@ -0,0 +1,21 @@
+package com.algorithms;
+
+import com.Board;
+
+public class RandomAlgorithm implements ChessAlgorithm {
+
+    @Override
+    public int[] play(Board board) {
+        int[][] moves = board.getLegalMoves();
+
+        // Get random number between 0 and moves.length
+        int random = (int) (Math.random() * moves.length);
+        return moves[random];
+    }
+
+    @Override
+    public int evaluate(Board board) {
+        throw new UnsupportedOperationException("Unimplemented method 'evaluate'");
+    }
+    
+}
